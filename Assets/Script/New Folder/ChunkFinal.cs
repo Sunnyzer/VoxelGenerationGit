fbce2d3c789f6c1dfc0cd5ac683e8964321fb51d
sunnyzer <nassim.ziouiche@gmail.com>
2022-10-14 18:17:43 +0200

    Revert "test normal en trop"
    This reverts commit 4396fc028d7aa9e16d43881b63913801baaca5fc.

--- Assets/Script/New Folder/ChunkFinal.cs
+++ Assets/Script/New Folder/ChunkFinal.cs
@@ -93,7 +93,7 @@ public class ChunkFinal : MonoBehaviour
         {
             if (item.Value.blockType != BlockType.Air) continue;
             Vector3 _direction = item.Key;
-            Vector3Int _faceCenter = _block.positionBlock + item.Key;
+            Vector3 _faceCenter = _block.positionBlock + _direction * (chunkParam.sizeBlock * 0.5f);
             Face _faceAddToMesh = meshData.AddFace(_faceCenter, item.Key);
             _block.AddNewFace(item.Key, _faceAddToMesh);
         }
@@ -146,10 +146,9 @@ public class ChunkFinal : MonoBehaviour
         meshData.DebugMesh();
         if (!blockData) return;
         Gizmos.DrawCube(BlockManager.Instance.GetBlockPositionWorldFromBlock(blockData), Vector3.one);
-        Gizmos.color = Color.red;
         foreach (var item in blockData.blocksNeighbor)
         {
-            Gizmos.DrawWireCube(BlockManager.Instance.GetBlockPositionWorldFromBlock(item.Value), Vector3.one);
+            Gizmos.DrawCube(BlockManager.Instance.GetBlockPositionWorldFromBlock(item.Value), Vector3.one);
         }
         foreach (var item in neighborChunk)
         {
